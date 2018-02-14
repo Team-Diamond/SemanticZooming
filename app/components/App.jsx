52c7d084ab0be0adc4b217fa590f601e9652eefc
Gubbala Madhuri <gubbalamadhuri1991@gmail.com>
2018-02-13 21:12:41 -0800

    Maximize app to screen size

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,7 +4,8 @@ import styles from '../style.css'
 export default class App extends React.Component {
     render() {
         return (
-            <div className={styles.WikiPage}>
+            <div>
+                {/*//className={styles.WikiPage}>*/}
                 <iframe src="https://en.wikipedia.org/wiki/French_Revolution"/>
             </div>);
     }
