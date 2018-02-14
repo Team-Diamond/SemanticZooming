d5a3d1059902a79da3fdc5dd0a1a7c4cf0860f74
Gubbala Madhuri <gubbalamadhuri1991@gmail.com>
2018-02-13 21:08:00 -0800

    Add css loader

---
+++ app/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../style.css'
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className={styles.WikiPage}>
+                <iframe src="https://en.wikipedia.org/wiki/French_Revolution"/>
+            </div>);
+    }
+}
