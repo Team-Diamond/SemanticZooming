724e6bb48fe9423ebaad6a309cf9add15bc7191d
Isaac Fehr <ifehr@ucsd.edu>
2018-03-03 16:05:59 -0800

    document now recursively styles child elements in wikipedia paragraphs

--- app/actions/Zoomable.js
+++ app/actions/Zoomable.js
@@ -1,17 +1,62 @@
 import React from 'react';
 
-/**
- * Takes a JSX element and adds inline styles based on the rules in the views of the component 
+/*
+ * Takes a JSX element and compiles a style for it based on the rules in the views of the component 
  *	and the current zoom level
  *
- * @param {JSX} element - the element to be styled
- * @param {[Object]} - the JSON views defined as such: 
+ * @param {String} className - the className of the element to be styled
+ * @param {String} tagName - the tagName of the element to be styled
+ * @param {[Object]} views - the JSON views defined as such: 
  *		[zoom:int, elements:[selector:string, styles:[name:string, min:float, max:float]]]
  * @param {float} zoom - the current zoom level on a scale from -2 to 2
+ *
+ * @returns {style} - an object containing style names and values that can be passed to a JSX element
  */
-export function zoom(element, views, zoom){
+export function zoomStyle(className, tagName, views, zoom){
 	var zoomLayer = Math.floor(zoom);
 	var zoomLayerPosition = zoom - zoomLayer;
+	var classNames = className.split(" ").find(x => x)
+	var style = {}
+	//Find the set of rules for styling this element, first by className, then by tagName
+	var elementsStyles = views.find(e => e.zoom == zoomLayer).elements;
+	var elementStyle = elementsStyles.find(s => classNames.find(s.class) );
+	if (!elementStyle) elementStyle = elementsStyles.find(s => s.tagName == element.tagName);
 
-	var elements = views.find(e => e.zoom == zoomLayer).elements;
+	if(elementStyle){
+		for(let attr in elementStyle.styles){
+			let name = attr.name;
+			let value = (attr.min + (attr.max - attr.min) * zoomLayerPosition) + attr.unit;
+			style.push({name, value});
+		}
+		return style;
+	} else{
+		return null;
+	}
+}
+
+/*
+ * Generates nest JSX elements from JSONified html elements
+ *
+ * @param {Object} elements - JSON containing information of nested HTML elements in the format:
+ *	{"tagName":String, "attributes": [{name:"String", value:value}], "children":[{repeat}]}
+ * @param {Object} views - JSON containing the styles at different levels of zoom for the current 
+ *	component
+ * @param {float} zoom - the current zoom level
+ *
+ * @returns {JSX} - a JSX element representing the input html styled according to the component's
+ *	views and the current zoom level.
+*/
+export function createZoomedElement(elements, views, zoom){
+	var currentElement = elements;
+	var TagName = element.tagName;
+	var className = element.className;
+	var attributes = element.attributes;
+	var children = elements.children;
+	var style = zoomStyle(className, TagName, views, zoom);
+
+	return (
+		<TagName className={className} style={style} {...attributes}>
+			{children && children.map((e) => e typeof String ? e : createZoomedElement(e, views, zoom))}
+		</TagName>
+	);
 }
--- app/components/Article.jsx
+++ app/components/Article.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import zoom from '../actions/Zoomable.js'
+import zoomStyle, createZoomedElement from '../actions/Zoomable.js'
 
 var views =[
 	{"zoom":0, "elements": [
 		{"selector": ".container", "styles" : [
-			{"name": "height", "min":100, "max":500},
-			{"name": "width", "min":50, "max": 100}
+			{"name": "height", "min":100, "max":500, "unit":"px"},
+			{"name": "width", "min":50, "max": 100, "unit":"px"}
 		]}
 	]},
 	{"zoom":1, "elements": [
@@ -26,19 +26,21 @@ export class Article extends React.Component{
 	render(){
 		const content = this.props.content;		
 		const title = content.title;
-		const outlineNumber = content.outline_number;
 		const thisLevelHTML = content.content_HTML;
-		const children = const.subsections;
 
-		const article = thisLevelHTML.map(function(element){
-			return zoom(element, views, this.props.zoom);
+		const sectionContent = thisLevelHTML.map(function(element){
+			return createZoomedElement(element, views, this.props.zoom);
+		});
+
+		const subsections = content.subsections.map(function(subsection, i){
+			return <Level1Subsection content={subsection[i]} zoom={this.props.zoom}/>;
 		});
 		
 		return (
-			<h2>{outlineNumber} {title}</h2>
-			{article}
-			<div className="contentBox">
-				{article}
+			<div className="article-container" style={zoomStyle("article-container", "div", this.props.zoom), views}>
+				<h2>{title}</h2>
+				{sectionContent}
+				{subsections}
 			</div>
 		);
 	}
